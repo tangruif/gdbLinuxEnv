eabd8c5ba0700e045fece399315f2c61dbe7dce1
tangruifeng <rf_tang@bupt.edu.cn>
2021-08-15 20:55:40 +0800

    edit bridge script

--- bridges/disable-bridge.sh
+++ bridges/disable-bridge.sh
@@ -2,16 +2,18 @@
 
 sudo brctl delif br0 tap0
 sudo brctl delif br0 tap1
+sudo brctl delif br0 tap2
+sudo brctl delif br0 tap3
 
 sudo tunctl -d tap0
 sudo tunctl -d tap1
 sudo tunctl -d tap2
 sudo tunctl -d tap3
 
-sudo brctl delif br0 $1
+#sudo brctl delif br0 $1
 
 sudo ifconfig br0 down
 sudo brctl delbr br0
 
-sudo ifconfig $1 up
-sudo dhclient -v $1
+#sudo ifconfig $1 up
+#sudo dhclient -v $1
--- bridges/enable-bridge.sh
+++ bridges/enable-bridge.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-if [ -z $1  ]
-then
-    echo "请输入网卡名称"
-    exit 1
-fi
-
-if [ -z "`ifconfig | grep $1`" ]
-then
-    echo "网卡${1}不存在"
-    exit 1
-fi
-
 sudo brctl addbr br0
-sudo ip addr flush dev $1
-sudo brctl addif br0 $1
 sudo tunctl -t tap0 -u `whoami`
 sudo tunctl -t tap1 -u `whoami`
 sudo tunctl -t tap2 -u `whoami`
@@ -24,11 +10,8 @@ sudo brctl addif br0 tap1
 sudo brctl addif br0 tap2
 sudo brctl addif br0 tap3
 
-sudo ifconfig $1 up
 sudo ifconfig tap0 up
 sudo ifconfig tap1 up
 sudo ifconfig tap2 up
 sudo ifconfig tap3 up
 sudo ifconfig br0 up
-
-sudo dhclient -v br0
